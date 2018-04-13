5322bd7c8b00a33b167ae2ab226238921384d697
李东东 <lidongdong2253@users.noreply.github.com>
2018-04-13 18:48:45 +0800

    web
    web

---
+++ build/web-mobile/src/settings.f5ef6.js
@@ -0,0 +1,132 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    rawAssets: {
+        assets: {
+            "e2VdOJJoJNeLvJ6d+4hEZ+": [
+                "Res/1111.png",
+                0
+            ],
+            "b0fy3KWsVB3rGugN1E5KOQ": [
+                "Res/2048.png",
+                0
+            ],
+            "c5FndyDgtMlr7hPL3WtFky": [
+                "Res/2222.png",
+                0
+            ],
+            "b7reDHFwZJLZ60+2cPgckV": [
+                "Res/3333.png",
+                0
+            ],
+            "11TbIMTfVLNYpHv4q8otff": [
+                "Res/4444.png",
+                0
+            ],
+            "b0gKv/NvtHQreYeaZpIpwv": [
+                "Res/5555.png",
+                0
+            ],
+            "4cXxTYrm5L2Lyk/IXag07V": [
+                "Res/WechatIMG34.png",
+                0
+            ],
+            "63Nm4UIVxINajYs/xzsPRZ": [
+                "Res/WechatIMG35.png",
+                0
+            ],
+            "95Gv306wVHT732Z0e85i+i": [
+                "Res/default_sprite_splash.png",
+                0
+            ],
+            "1dAt9o3NREOIS1Atu9ubSY": [
+                "Res/gameBg.png",
+                0
+            ]
+        },
+        internal: {
+            "71VhFCTINJM6/Ky3oX9nBT": [
+                "image/default_btn_disabled.png",
+                0
+            ],
+            "e8Ueib+qJEhL6mXAHdnwbi": [
+                "image/default_btn_normal.png",
+                0
+            ],
+            "b4P/PCArtIdIH38t6mlw8Y": [
+                "image/default_btn_pressed.png",
+                0
+            ]
+        }
+    },
+    assetTypes: [
+        "cc.Texture2D"
+    ],
+    launchScene: "db://assets/Scenes/LoginScene.fire",
+    scenes: [
+        {
+            url: "db://assets/Scenes/LoginScene.fire",
+            uuid: 2
+        },
+        {
+            url: "db://assets/Scenes/Gamescene.fire",
+            uuid: 0
+        }
+    ],
+    packedAssets: {
+        "03ddecce6": [
+            1,
+            "437DYJfWZMjr0nyjbcgtXV",
+            "68JLW/kCZGF7EW18pDbEcO",
+            "791fdaq1FAk7JzA3Pqv2yw",
+            "9byKYzQb5JyoVArrg1iKwu",
+            "ad/mXye35EEpCQXcNA+PAo",
+            3,
+            "bfY2zgmsNBjbbCa2uO/CGq",
+            "daIji6s7pJy5zVhSqpmU87",
+            0,
+            "f7D5rg3aJDIYcUqL10PD8K"
+        ],
+        "05db9ebbc": [
+            "29FYIk+N1GYaeWH/q1NxQO",
+            1,
+            2,
+            3,
+            "e97GVMl6JHh5Ml5qEDdSGa",
+            "ebnleSpcFFvKZwt03EOZDb",
+            "f0BIwQ8D5Ml7nTNQbh1YlS",
+            "fazGnwMtBNiJTKcwObLBmz"
+        ]
+    },
+    orientation: "portrait",
+    uuids: [
+        "e2e4TANUpPBpSbsoJbdPSr",
+        "41JIseIH5CVa4tMqUkQ3uM",
+        "5avZIN+bxAzLrZX+djH1uF",
+        "b7B55bOA9DFqwt2upbV6+f"
+    ],
+    "md5AssetsMap": {
+        "03/03ddecce6.json": "73877",
+        "05/05db9ebbc.json": "e5f46",
+        "assets/Res/1111.png": "09f7a",
+        "assets/Res/2048.png": "0b0b3",
+        "assets/Res/2222.png": "9896f",
+        "assets/Res/3333.png": "09dbe",
+        "assets/Res/4444.png": "779c2",
+        "assets/Res/5555.png": "31121",
+        "assets/Res/default_sprite_splash.png": "cea68",
+        "assets/Res/gameBg.png": "e40eb",
+        "assets/Res/WechatIMG34.png": "4f3da",
+        "assets/Res/WechatIMG35.png": "9f0b2",
+        "internal/image/default_btn_disabled.png": "286c6",
+        "internal/image/default_btn_normal.png": "1ecb7",
+        "internal/image/default_btn_pressed.png": "bedf4"
+    }
+};
